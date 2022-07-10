f44fc14673bfc6ddbe8287bc2eaebf6c1f0fa236
yusuf963 <mohammad963yusuf@gmail.com>
2022-07-10 18:32:58 +0100

    adding

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { Navigation } from "./components/navigation/Navigation";
+import { Home } from "./pages/Home";
+import { SignIn } from "./pages/SignIn";
 
-function App() {
+const Shop = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>for the text shop</p>
     </div>
   );
-}
+};
+const App = () => {
+  return (
+    <p>Hello</p>
+    // <Routes>
+    //   <Route path="/" element={<Navigation />}>
+    //     <Route index element={<Home />} />
+    //     <Route path="/shop" element={<Shop />} />
+    //     <Route path="/sign-in" element={<SignIn />} />
+    //   </Route>
+    // </Routes>
+  );
+};
 
-export default App;
+export { App, Shop };
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import {Directory} from '../components/directory/Directory'
+
+const Home = () => {
+    return (
+        <div>
+            <Directory/> 
+        </div>
+    );
+}
+
+export {Home};
